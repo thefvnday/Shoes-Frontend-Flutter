48b7619c42354d4ebabccb30e343a94ee29801e2
Fandy Ahmad Irianto <fandy.09.ahmad@gmail.com>
2021-10-11 21:57:45 +0700

    menambahkan icon medsos di login screen

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:sepasang/Screens/Home/home_screen.dart';
 import 'package:sepasang/Screens/Login/components/background.dart';
+import 'package:sepasang/Screens/Signup/components/social_icon.dart';
 import 'package:sepasang/Screens/Signup/signup_screen.dart';
 import 'package:sepasang/components/already_have_an_account.dart';
 import 'package:sepasang/components/rounded_button.dart';
 import 'package:sepasang/components/rounded_input_field.dart';
 import 'package:sepasang/components/rounded_password_field.dart.dart';
 
-
 class Body extends StatefulWidget {
-
   @override
   _BodyState createState() => _BodyState();
 }
@@ -31,7 +30,6 @@ class _BodyState extends State<Body> {
                 fontSize: 20,
                 color: Colors.purple),
           ),
-        
           Positioned(
             bottom: 0,
             left: 0,
@@ -40,13 +38,12 @@ class _BodyState extends State<Body> {
               width: size.width * 0.5,
             ),
           ),
-         
           RoundedInputField(
             hinText: "Your Email",
             onChanged: (value) {},
           ),
           RoundedPasswordField(
-            onshowpassword: (){
+            onshowpassword: () {
               setState(() {
                 isfisible = !isfisible;
               });
@@ -56,33 +53,52 @@ class _BodyState extends State<Body> {
           ),
           RoundedButton(
             text: "LOGIN",
-            press: (){
-               Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return HomeScreen();
-                      },
-                    ),
-                  );
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return HomeScreen();
+                  },
+                ),
+              );
             },
           ),
-         
           AlreadyHaveAnAccountCheck(
             press: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return SignupScreen();
-                        },
-                      ),
-                    );
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SignupScreen();
                   },
-          )
+                ),
+              );
+            },
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                
+                SocialIcon(
+                  iconSrc: "assets/images/facebook.png",
+                  press: (){},
+                ),
+                
+                SocialIcon(
+                  iconSrc: "assets/images/google.png",
+                  press: (){},
+                ),
+               
+                SocialIcon(
+                  iconSrc: "assets/images/twitter.png",
+                  press: (){},
+                ),
+
+              ],
+            )
         ],
       ),
     );
   }
 }
-
